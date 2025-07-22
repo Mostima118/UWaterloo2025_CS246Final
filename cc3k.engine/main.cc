65384676736b316c36047414c5c555db1e045535
06Lelouch <ewise0628@gmail.com>
2025-07-22 12:27:53 -0400

    deleted developing flags

--- cc3k.engine/main.cc
+++ cc3k.engine/main.cc
@@ -7,8 +7,6 @@ int main(int argc, char* argv[]) {
     std::string layoutFile;            // empty => randomized
     unsigned seed = 0;                 // 0 => time-based seed
 
-    // Todo: parse command-line arguments for race, layoutFile, and seed
-
     cc3k::GameEngine engine{layoutFile, seed};
     engine.run();
     return 0;
