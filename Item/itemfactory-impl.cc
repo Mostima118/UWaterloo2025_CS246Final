8bbe5ff83458c5e36882ca42146d3cb54063fa27
Wilson Kuang <w6kuang@uwaterloo.ca>
2025-07-23 17:02:16 -0400

    fixed item related classes to fit the game engine, updated annotations

--- Item/itemfactory-impl.cc
+++ Item/itemfactory-impl.cc
@@ -8,10 +8,14 @@ std::unique_ptr<Item> createPreset(char itemType) {
     return Item::createPreset(itemType);
 }
 
-std::unique_ptr<Item> ItemFactory::createRandomPotion() {
-    return Item::createRandomPotion();
+std::unique_ptr<Item> ItemFactory::createRandomPotion(unsigned seed) {
+    return Item::createRandomPotion(seed);
 }
 
-std::unique_ptr<Item> ItemFactory::createRandomTreasure() {
-    return Item::createRandomTreasure();
+std::unique_ptr<Item> ItemFactory::createRandomTreasure(unsigned seed) {
+    return Item::createRandomTreasure(seed);
+}
+
+std::vector<std::unique_ptr<Item>> Item::createGold(char c) {
+    return Item::createGold(c);
 }
--- Item/itemfactory.cc
+++ Item/itemfactory.cc
@@ -6,6 +6,7 @@ import item;
 export class ItemFactory {
 public:
     static std::unique_ptr<Item> createPreset(char itemType);
-    static std::unique_ptr<Item> createRandomPotion();
-    static std::unique_ptr<Item> createRandomTreasure();
+    static std::unique_ptr<Item> createRandomPotion(unsigned seed);
+    static std::unique_ptr<Item> createRandomTreasure(unsigned seed);
+    static std::vector<std::unique_ptr<Item>> createGold(char c);
 };
