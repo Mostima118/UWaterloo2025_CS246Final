e74ec903844b0e91a944617dd955dc7d0d99e7af
Wilson Kuang <w6kuang@uwaterloo.ca>
2025-07-23 16:55:01 -0400

    fixed item related classes to fit the game engine, updated annotations

--- Item/item.cc
+++ Item/item.cc
@@ -1,6 +1,7 @@
 export module item;
 
 import <memory>;
+import <vector>;
 
 export class Item {
 protected:
@@ -14,15 +15,17 @@ public:
     int getX() const;
     int getY() const;
 
-    // Reset the postion
+    // Reset the position
     void setPosition(int x, int y);
 
     // getType will return the preset number ('0' - '9') of each item
-    virtual void getType() = 0;
+    virtual char getType() = 0;
 
     // use will return a char to represent the potion 
     //  or an int of it's applied on treasure
-    virtual void use() = 0;
+    // NEED TO BE FIXED //
+    // Either overload use or make it return the same type
+    virtual char use() = 0;
 
     // Symbol representation of potion "P" or treasure "G"
     virtual char getSymbol() const = 0;
@@ -30,7 +33,7 @@ public:
     virtual bool isPotion() const;
 
     // Create item based on the preset ('0' - '9'), used for testing purposes
-    std::unique_ptr<Item> createPreset(char itemType);
+    static std::unique_ptr<Item> createPreset(char itemType);
 
     // Random generation, potion
     static std::unique_ptr<Item> createRandomPotion(unsigned seed = 0);
@@ -38,6 +41,7 @@ public:
     // Random generation, treasure
     static std::unique_ptr<Item> createRandomTreasure(unsigned seed = 0);
         
-    // Add makeUnique that create two piles of normal gold when human is killed
+    // createGold creates two piles of normal gold when human is killed
+    //  and one pile of merchant hoard when merchant is killed
     static std::unique_ptr<Item> createGold(char c);
 };
