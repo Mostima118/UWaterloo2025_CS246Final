70dace93883d257b2e7a545c3a3c0e92df467af9
06Lelouch <ewise0628@gmail.com>
2025-07-23 20:05:18 -0400

    update to v0.2
    updated engine logic

--- cc3k.engine/floor_generator.cc
+++ cc3k.engine/floor_generator.cc
@@ -10,15 +10,10 @@ namespace cc3k {
 export class FloorGenerator {
 public:
     FloorGenerator() = default;
-    //load layout from file(base layout)
     void loadLayout(const std::string& filename);
-    //generate randomfloor (do not support turn on/off in case of pre-rendered maps)
     void generateRandomFloor(unsigned seed = 0);
-    //getMap()
     const std::vector<std::string>& getMap() const;
-    //item spawn
     std::vector<std::unique_ptr<Item>> spawnItems(unsigned seed) const;
-    //enemy spawn
     std::vector<std::unique_ptr<Enemy>> spawnEnemies(unsigned seed) const;
 };
 private:
